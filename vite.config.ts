a52d59b1df4d69111bac290b982064295796acff
Axel <axelelhilon@gmail.com>
2025-04-22 17:22:49 -0300

    Configuracion de Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
+  base: './', // 🟣 Esto es CLAVE para rutas correctas en producción
   plugins: [
     tailwindcss(),
   ],
